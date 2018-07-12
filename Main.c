54c3bfd570ed5cb16a676858bacdf16dd68b2638
danial-abadi <danial.abadi.ghadim@gmail.com>
2018-07-13 02:19:01 +0430

    add a simple login menu and get user input and check its validate functions

--- Main.c
+++ Main.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
+#include <string.h>
 
+char* un ="admin";
+char* ps="admin";
+//----------------------------------------
+
+/**
+*check entered username and password.
+*/
+int isValidUser(char* username,char* password)
+{
+		if(!strcmp(username,un)&&!strcmp(password,ps))
+		{
+			return 1;	
+		}else
+		{	
+			return 0;
+		}
+}
+
+
+int login()
+{
+		char username[10];
+		char password[10];
+		//
+		printf("login\n");
+		printf("username:");
+		scanf("%s" ,username);
+		printf("password:");
+		scanf("%s" ,password);
+		//
+		short isValid = isValidUser(username,password);
+		if(isValid==1)
+		{
+			printf("ok, you are in");
+		}else
+		{
+			printf("username or password is wrong!");
+		}
+		//
+		return isValid;
+}
+
+
+
+
+
+/**
+*main method
+*/
 int main ()
 	{
-		printf("it is worked");
+		
+		login();
+		
+		return 0;
 	}
+
+
+
+
+
+
+
+
+
+
+
